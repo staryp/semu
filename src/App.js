e69c0c50b80b49c5648245bc342f5b8084f82496
staryp <staryp3@gmail.com>
2020-09-19 07:47:21 +0900

    semu process bigin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import FindSemuProcess from './FindSemuProcess';
 
-function App() {
-  return (
+class App extends Component {
+  render () {
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">
+          <img src='icon/menu.png' className="menu" alt="logo" width="30"/>
+        </Navbar.Brand>
+        
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            세무사앱
+          </Nav>
+          <Form inline>
+            <img src='icon/alarm.png' className="menu" alt="logo" width="30"/>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+      <FindSemuProcess></FindSemuProcess>
+    </div> 
+    );
+  }
 }
 
 export default App;
---
+++ src/FindSemuProcess.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+class FindSemuProcess extends Component {
+    render () {
+      return(
+      <div className="FindSemu">
+        세무 프로세스
+        <h4>어떤 견적을 받아 보시겠습니까?</h4>
+      </div> 
+      );
+    }
+  }
+  
+  export default FindSemuProcess;
