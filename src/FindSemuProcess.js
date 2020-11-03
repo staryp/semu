f1352c90bf1ba3f97d593c89162fb34a55d45b92
staryp <staryp3@gmail.com>
2020-11-03 10:56:48 +0900

    6

--- src/FindSemuProcess.js
+++ src/FindSemuProcess.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -9,6 +10,15 @@ class FindSemuProcess extends Component {
       <div className="FindSemu">
         세무 프로세스
         <h4>어떤 견적을 받아 보시겠습니까?</h4>
+        <ListGroup>
+        <ListGroup.Item>기장(매월관리)</ListGroup.Item>
+        <ListGroup.Item>부가가치세 신고 대행(1회성)</ListGroup.Item>
+        <ListGroup.Item>종합소득세 신고 대행(1회성)</ListGroup.Item>
+        <ListGroup.Item>무효법인설립</ListGroup.Item>
+        <ListGroup.Item>양도 소득세 신고(상담 유료)</ListGroup.Item>
+        <ListGroup.Item>상속세 신고</ListGroup.Item>
+        <ListGroup.Item>기타</ListGroup.Item>
+        </ListGroup>
       </div> 
       );
     }
